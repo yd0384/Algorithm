0fa299615dca0fdde37efd1f52a5636f6d9c7913
yd0384 <youngdeok9@naver.com>
2021-02-06 18:22:31 +0900

    Create BJ2751_수_정렬하기.py

---
+++ BJ2751_수_정렬하기.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  6 17:06:29 2021
+
+@author: young
+"""
+import sys
+MAX_NUM=2000001
+N=int(sys.stdin.readline())
+seq=list()
+for n in range(N):
+    seq.append(int(sys.stdin.readline()))
+count=[0]*MAX_NUM
+for i in seq:
+    count[i+1000000]=1
+for i in range(MAX_NUM):
+    if count[i]==1:
+        print(i-1000000)
