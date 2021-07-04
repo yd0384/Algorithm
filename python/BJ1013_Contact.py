3cbd3957ebae1985a0358129457265308c0970a7
yd0384 <youngdeok9@naver.com>
2021-07-04 15:46:14 +0900

    python java 나누기

---
+++ python/BJ1013_Contact.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 24 17:58:49 2021
+
+@author: yd0384
+@Problem: https://www.acmicpc.net/problem/1013
+given pattern: (100+1+ | 01)+
+"""
+
+import sys
+import re
+
+T=int(sys.stdin.readline())
+result=list()
+for t in range(T):
+    line = sys.stdin.readline().strip()
+    if line[-1]=='0':
+        r=False
+        result.append(r)
+        continue
+    start=True
+    i=0
+    zero=True
+    r=False
+    while i<len(line):
+        if start:
+            if line[i]=='0':
+                if line[i+1]=='0':
+                    if i-2>0 and line[i-2:i+2]=='1100':
+                        i=i+2
+                        start=False
+                        zero=True
+                        continue
+                    r=False
+                    break
+                i=i+2
+                continue
+            else:
+                if line[i:i+3]!='100':
+                    r=False
+                    break
+                i=i+3
+                start=False
+                continue
+        elif zero:
+            if line[i]=='1':
+                zero=False
+
+        else:
+            if line[i]=='0':
+                zero=True
+                start=True
+                continue
+        i+=1
+    if i==len(line):
+        r=True
+    result.append(r)
+for r in (result):       
+    if r:
+        print('Yes')
+    else:
+        print('No')
