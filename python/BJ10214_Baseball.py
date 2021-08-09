c08f26bcc234bc74581462ef03f261b27e632bff
yd0384 <youngdeok9@naver.com>
2021-08-09 14:15:40 +0900

    0809

--- python/BJ10214_Baseball.py
+++ python/BJ10214_Baseball.py
@@ -1,23 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 28 15:03:26 2019
-
-@author: Young Deok
-"""
-
 T=int(input())
-
 for t in range(T):
     Y=0
     K=0
     for i in range(9):
         s=input().split()
-        y=s[0]
-        k=s[1]
-        if y>k:
-            Y+=1
-        elif y<k:
-            K+=1
+        y=int(s[0])
+        k=int(s[1])
+        Y+=y
+        K+=k
     if Y>K:
         print("Yonsei")
     elif Y==K:
