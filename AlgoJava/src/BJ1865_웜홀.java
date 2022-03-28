1eb267d69ad48d7eddcf5037a580b27b556ccd0f
yd0384 <youngdeok9@naver.com>
2022-03-29 00:55:10 +0900

    2022/03/29

--- AlgoJava/src/BJ1865_웜홀.java
+++ AlgoJava/src/BJ1865_웜홀.java
@@ -1,71 +1,95 @@
 import java.util.*;
 import java.io.*;
 public class BJ1865_웜홀 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        for (int t = 0; t < T; t++) {
+        int TC = Integer.parseInt(br.readLine());
+        for (int tc = 0; tc < TC; tc++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int N, M, W;
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-            W = Integer.parseInt(st.nextToken());
-            List<Map<Integer, Integer>> map = new ArrayList<>();
-            for(int i = 0; i <= N; i++){
-                map.add(new HashMap<>());
-            }
-            for (int i = 0; i < M; i++) {
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+            Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
+            for (int m = 0; m < M; m++) {
                 st = new StringTokenizer(br.readLine());
-                int u, v, w;
-                u = Integer.parseInt(st.nextToken());
-                v = Integer.parseInt(st.nextToken());
-                w = Integer.parseInt(st.nextToken());
-                if(map.get(u).get(v)==null){
-                    map.get(u).put(v, w);
+                int S = Integer.parseInt(st.nextToken());
+                int E = Integer.parseInt(st.nextToken());
+                int T = Integer.parseInt(st.nextToken());
+                if(map.get(S) == null) {
+                    Map<Integer, Integer> tmpMap = new HashMap<>();
+                    tmpMap.put(E, T);
+                    map.put(S, tmpMap);
                 }
-                else if(map.get(u).get(v) > w){
-                    map.get(u).put(v, w);
+                else if(map.get(S).get(E) == null) {
+                    map.get(S).put(E, T);
+                }
+                else if (map.get(S).get(E) > T) {
+                    map.get(S).put(E, T);
+                }
+                if(map.get(E) == null) {
+                    Map<Integer, Integer> tmpMap = new HashMap<>();
+                    tmpMap.put(S, T);
+                    map.put(E, tmpMap);
+                }
+                else if(map.get(E).get(S) == null) {
+                    map.get(E).put(S, T);
+                }
+                else if (map.get(E).get(S) > T) {
+                    map.get(E).put(S, T);
                 }
             }
-            for (int i = M; i < M+W; i++) {
+            for (int w = 0; w < W; w++) {
                 st = new StringTokenizer(br.readLine());
-                int u, v, w;
-                u = Integer.parseInt(st.nextToken());
-                v = Integer.parseInt(st.nextToken());
-                w = Integer.parseInt(st.nextToken());
-                if(map.get(u).get(v)==null){
-                    map.get(u).put(v, -w);
+                int S = Integer.parseInt(st.nextToken());
+                int E = Integer.parseInt(st.nextToken());
+                int T = Integer.parseInt(st.nextToken());
+                T = -T;
+                if(map.get(S) == null) {
+                    Map<Integer, Integer> tmpMap = new HashMap<>();
+                    tmpMap.put(E, T);
+                    map.put(S, tmpMap);
                 }
-                else if(map.get(u).get(v) > -w){
-                    map.get(u).put(v, -w);
+                else if(map.get(S).get(E) == null) {
+                    map.get(S).put(E, T);
+                }
+                else if (map.get(S).get(E) > T) {
+                    map.get(S).put(E, T);
                 }
             }
-            final int INF = 987654321;
             int[] D = new int[N+1];
-            for(int i=1; i<=N; i++){
-                D[i] = INF;
-                map.get(0).put(i, 0);
-            }
-            for(int i=0; i<N-1; i++){
-                for (int j = 0; j <= N; j++) {
-                    for(int key : map.get(j).keySet()){
-                        if(D[key] > D[j] + map.get(j).get(key)){
-                            D[key] = D[j] + map.get(j).get(key);
+            for (int i = 0; i < N-1; i++) {
+                for(Map.Entry<Integer, Map<Integer, Integer>> entry : map.entrySet()) {
+                    int start = entry.getKey();
+                    for(Map.Entry<Integer, Integer> innerEntry : entry.getValue().entrySet()) {
+                        int end = innerEntry.getKey();
+                        int weight = innerEntry.getValue();
+                        if(D[end] > D[start] + weight) {
+                            D[end] = D[start] + weight;
                         }
-                    }    
+                    }
                 }
             }
             boolean changed = false;
-            for (int j = 0; j <= N; j++) {
-                for(int key : map.get(j).keySet()){
-                    if(D[key] > D[j] + map.get(j).get(key)){
-                        D[key] = D[j] + map.get(j).get(key);
+            for(Map.Entry<Integer, Map<Integer, Integer>> entry : map.entrySet()) {
+                int start = entry.getKey();
+                for(Map.Entry<Integer, Integer> innerEntry : entry.getValue().entrySet()) {
+                    int end = innerEntry.getKey();
+                    int weight = innerEntry.getValue();
+                    if(D[end] > D[start] + weight) {
                         changed = true;
                         break;
                     }
-                }    
+                }
+                if(changed) {
+                    break;
+                }
             }
-            System.out.println(changed?"YES":"NO");
+            if(changed) {
+                System.out.println("YES");
+            }
+            else {
+                System.out.println("NO");
+            }
         }
     }
 }
